0daf9e8c73732bb94ac691ac54ae0edf97319e21
huttona6 <huttona6@uni.coventry.ac.uk>
2021-09-25 17:20:01 +0100

    Forth Challenge Completed

--- 01-Fundamentals/script.js
+++ 01-Fundamentals/script.js
@@ -64,6 +64,11 @@ if (dolphinScore2 > koalasScore2 || dolphinScore2 < koalasScore2){
 
 //Forth Challenge
 
-//Practice Code
+const bill = 275;
+const tip = bill <= 300 && bill >= 50 ? bill * 0.15:
+bill * 0.2
+console.log(`The bill was ${bill}, the tip was ${tip}
+and the total value is ${bill + tip}`)
+
 
 
