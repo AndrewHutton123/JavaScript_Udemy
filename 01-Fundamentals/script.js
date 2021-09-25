a5ac9332ea428c2d74d02305bbb4490b6477e27c
huttona6 <huttona6@uni.coventry.ac.uk>
2021-09-25 13:37:08 +0100

    Made Seperate JS File

---
+++ 01-Fundamentals/script.js
@@ -0,0 +1,12 @@
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+
+let firstName = "Jonas";
+console.log(firstName);
+
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'Teacher';
+console.log(myFirstJob);
+console.log(myCurrentJob);
