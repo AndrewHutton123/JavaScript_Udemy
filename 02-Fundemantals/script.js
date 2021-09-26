f5ecd07b625fc430bea395e6af17fecf1a2d887a
huttona6 <huttona6@uni.coventry.ac.uk>
2021-09-26 13:54:43 +0100

    Started 02-Fundementals

---
+++ 02-Fundemantals/script.js
@@ -0,0 +1,17 @@
+'use strict';
+
+//First Challenge
+
+//Second Challenge
+
+//Third Challenge
+
+//Forth Challenge
+
+//Practice Code
+
+function logger() {
+    console.log('My Name is Andy');
+}
+
+logger();
