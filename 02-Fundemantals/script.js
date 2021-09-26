c02eb3c567b7d06f3ba6162796e0fc9c3630bf4a
huttona6 <huttona6@uni.coventry.ac.uk>
2021-09-26 14:25:12 +0100

    Working on Functions

--- 02-Fundemantals/script.js
+++ 02-Fundemantals/script.js
@@ -8,10 +8,41 @@
 
 //Forth Challenge
 
-//Practice Code
-
-function logger() {
-    console.log('My Name is Andy');
+/*Function declaration
+function fruitProcessor(apples, oranges) {
+    console.log(apples,oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+    return juice;
 }
 
-logger();
+const appleJuice = fruitProcessor(3,4);
+console.log(appleJuice);
+*/
+//Fucntion expression
+const calcAge1 = function(birthYear){
+    const age = 2021 - birthYear;
+    return age;
+}
+
+//Arrow Function
+const yearsUntilRetirment = (birthYear,firstName) => {
+    const age = 2021 - birthYear;
+    const retirment = 65 - age;
+    return `${firstName} retires in ${retirment} years`;
+}
+console.log(yearsUntilRetirment(1999, 'Andy'));
+
+//Functions Calling Other Functions
+
+function cutFruitPieces(fruit){
+    return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+    const applePieces = cutFruitPieces(apples);
+    const orangePieces = cutFruitPieces(oranges);
+    const juice = `Juice with ${applePieces} apples and ${orangePieces} oranges`;
+    return juice;
+}
+
+console.log(fruitProcessor(4,5));
