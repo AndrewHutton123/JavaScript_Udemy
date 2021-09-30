1ea856e3d5993ee94bdcd383a10417041e86ae2f
huttona6 <huttona6@uni.coventry.ac.uk>
2021-09-30 12:06:47 +0100

    Using API calls

--- 03-Asynchronous/script.js
+++ 03-Asynchronous/script.js
@@ -4,3 +4,29 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+
+const request = new XMLHttpRequest();
+request.open(`GET`, `https://restcountries.com/v3.1/name/britain`);
+request.send();
+//The response is in JSON
+console.log(request.responseText);
+
+request.addEventListener(`load`, function(){
+    //Convererts JSON into and object and destructs it
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    const html = `
+    <article class="country">
+    <img class="country__img" src="${data.flag}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)} people</p>
+      <p class="country__row"><span>🗣️</span>${data.languages[0]}</p>
+      <p class="country__row"><span>💰</span>${data.currencies[0]}</p>
+    </div>
+  </article>`;
+  countriesContainer.insertAdjacentHTML('beforeend',html);
+  countriesContainer.getElementsByClassName.opacity = 1;
+});
